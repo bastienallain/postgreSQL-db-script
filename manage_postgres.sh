874a0437fe867e767cb693968986288850ad9c5e
Vivacoda <75243836+vivacoda@users.noreply.github.com>
2023-12-29 17:10:32 +0700

    Update prompt

--- manage_postgres.sh
+++ manage_postgres.sh
@@ -35,38 +35,38 @@ create_db_and_user() {
 }
 
 # Function to create a user for an existing database
-echo "List of existing databases:"
-sudo -u postgres psql -c '\l'
+create_user_for_existing_db() {
+    echo "List of existing databases:"
+    sudo -u postgres psql -c '\l'
 
-read -p "Enter the name of the existing database: " existing_db
-read -p "Enter the prefix for the user: " user_prefix
+    read -p "Enter the name of the existing database: " existing_db
+    read -p "Enter the prefix for the user: " user_prefix
 
-local user_suffix=$(generate_random_suffix 80)
-local password=$(generate_random_suffix 90)
+    local user_suffix=$(generate_random_suffix 80)
+    local password=$(generate_random_suffix 90)
 
-local user_name="$(sanitize_input "$user_prefix")_${user_suffix}"
+    local user_name="$(sanitize_input "$user_prefix")_${user_suffix}"
 
-sudo -u postgres psql -c "CREATE USER $user_name WITH PASSWORD '$password';"
-sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $existing_db TO $user_name;"
+    sudo -u postgres psql -c "CREATE USER $user_name WITH PASSWORD '$password';"
+    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $existing_db TO $user_name;"
 
-echo "DATABASE_URL=postgresql://$user_name:$password@localhost:5432/$existing_db"
-echo "DATABASE: $existing_db"
-echo "username: $user_name"
-echo "password: $password"
+    echo "DATABASE_URL=postgresql://$user_name:$password@localhost:5432/$existing_db"
+    echo "DATABASE: $existing_db"
+    echo "username: $user_name"
+    echo "password: $password"
 }
 
-
-
+# Main menu
 while true; do
-    echo "1: Création de DB et Users avec GRANT ALL PRIVILEGES"
-    echo "2: Créer un user pour une DB existante"
-    echo "3: Quitter"
-    read -p "Choisissez une option: " choice
+    echo "1: Create DB and Users with GRANT ALL PRIVILEGES"
+    echo "2: Create a user for an existing DB"
+    echo "3: Exit"
+    read -p "Choose an option: " choice
 
     case $choice in
         1) create_db_and_user ;;
         2) create_user_for_existing_db ;;
         3) exit ;;
-        *) echo "Option invalide. Réessayez." ;;
+        *) echo "Invalid option. Please try again." ;;
     esac
 done
